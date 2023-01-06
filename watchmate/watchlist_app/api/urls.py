4c863c0db9ed7cacc2988725dcbd017f3d5bab93
vishalDatodiya <vishaldatodiya12@gmail.com>
2023-01-07 00:51:13 +0530

    Commented the Fuunction Based View. and Use the Class Based View.

--- watchmate/watchlist_app/api/urls.py
+++ watchmate/watchlist_app/api/urls.py
@@ -2,6 +2,14 @@ from django.urls import path
 from watchlist_app.api.views import WatchListView, WatchListDetail
 
 urlpatterns = [
-    path('', WatchListView, name='watchlist'),
-    path('<int:pk>/', WatchListDetail, name='watchlistDetail'),
+    
+    # Class based View Urls
+    path('', WatchListView.as_view(), name="watchlist"),
+    path('<int:pk>/', WatchListDetail.as_view(), name="watchListDetail"),
+    
+    
+    # Function Based View Urls
+    
+#     path('', WatchListView, name='watchlist'),
+#     path('<int:pk>/', WatchListDetail, name='watchlistDetail'),
 ]
--- watchmate/watchlist_app/api/views.py
+++ watchmate/watchlist_app/api/views.py
@@ -1,31 +1,30 @@
 from watchlist_app.models import WatchList
 from watchlist_app.api.serializers import WatchListSerializer
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+# from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework import status
 
 
-@api_view(['GET','post'])
-def WatchListView(request):
+class WatchListView(APIView):
     
-    if request.method == 'GET':
+    def get(self, request):
         wathlist = WatchList.objects.all()
         serializer = WatchListSerializer(wathlist, many=True)
         return Response(serializer.data)
-    
-    if request.method == 'POST':
+
+    def post(self, request):
         serializer = WatchListSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         else:
             return Response(serializer.errors)
+
+
+class WatchListDetail(APIView):
     
-    
-@api_view(['GET','PUT','DELETE'])
-def WatchListDetail(request, pk):
-    
-    if request.method == 'GET': 
+    def get(self, request, pk):
         try:
             watchlist = WatchList.objects.get(pk=pk)
         except:
@@ -33,7 +32,7 @@ def WatchListDetail(request, pk):
         serializer = WatchListSerializer(watchlist)
         return Response(serializer.data)
 
-    if request.method == 'PUT':
+    def put(self, request, pk):
         movie = WatchList.objects.get(pk=pk)
         serializer = WatchListSerializer(movie, data=request.data)
         if serializer.is_valid():
@@ -41,11 +40,60 @@ def WatchListDetail(request, pk):
             return Response(serializer.data)
         else:
             return Response(serializer.errors)
-    
-    if request.method == 'DELETE':
-        movie = WatchList.objects.get(pk=pk)
+        
+    def delete(self, request, pk):
+        try:
+            movie = WatchList.objects.get(pk=pk)
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
         movie.delete()
         data = {
             'detail':"Deleted successfully",
         }
         return Response(data, status=status.HTTP_204_NO_CONTENT)
+
+
+# @api_view(['GET','post'])
+# def WatchListView(request):
+    
+#     if request.method == 'GET':
+#         wathlist = WatchList.objects.all()
+#         serializer = WatchListSerializer(wathlist, many=True)
+#         return Response(serializer.data)
+    
+#     if request.method == 'POST':
+#         serializer = WatchListSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         else:
+#             return Response(serializer.errors)
+    
+    
+# @api_view(['GET','PUT','DELETE'])
+# def WatchListDetail(request, pk):
+    
+#     if request.method == 'GET': 
+#         try:
+#             watchlist = WatchList.objects.get(pk=pk)
+#         except:
+#             return Response(status=status.HTTP_404_NOT_FOUND)
+#         serializer = WatchListSerializer(watchlist)
+#         return Response(serializer.data)
+
+#     if request.method == 'PUT':
+#         movie = WatchList.objects.get(pk=pk)
+#         serializer = WatchListSerializer(movie, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         else:
+#             return Response(serializer.errors)
+    
+#     if request.method == 'DELETE':
+#         movie = WatchList.objects.get(pk=pk)
+#         movie.delete()
+#         data = {
+#             'detail':"Deleted successfully",
+#         }
+#         return Response(data, status=status.HTTP_204_NO_CONTENT)
