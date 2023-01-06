dc3ab51435f411f9c7674d2eec9ddb7f494d40a9
vishalDatodiya <vishaldatodiya12@gmail.com>
2023-01-07 01:10:50 +0530

    Using ModelSerializer instead of Serializer. I commented all the classes of serializers.py

--- watchmate/watchlist_app/api/serializers.py
+++ watchmate/watchlist_app/api/serializers.py
@@ -1,36 +1,53 @@
 from rest_framework import serializers
-# from rest_framework.validators import ValidationError
 from watchlist_app.models import WatchList
 
-class WatchListSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField()
-    storyline = serializers.CharField()
-    active = serializers.BooleanField(default=False)
-    created_date = serializers.DateTimeField()
+
+class WatchListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = WatchList
+        # fields = "__all__"
+        # or
+        # fields = ['title','storyline','active','created_date']
+        
+        # or if I want to exclude some fields then
+        exclude = ('created_date',)
+
+    # We can use validators here
+    
+    
+    
+
+# If we are using ModelSerializer Then we Don't need of any create update or manually defined the fields name
+
+# class WatchListSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     title = serializers.CharField()
+#     storyline = serializers.CharField()
+#     active = serializers.BooleanField(default=False)
+#     created_date = serializers.DateTimeField()
     
         
-    def create(self, validated_data):
-        return WatchList.objects.create(**validated_data)
+#     def create(self, validated_data):
+#         return WatchList.objects.create(**validated_data)
         
         
-    def update(self, instance, validate_data):
-        instance.title = validate_data.get('title', instance.title)
-        instance.storyline = validate_data.get('storyline', instance.storyline)
-        instance.active = validate_data.get('active', instance.active)
-        instance.created_date = validate_data.get('created_date', instance.created_date)
-        instance.save()
-        return instance
+#     def update(self, instance, validate_data):
+#         instance.title = validate_data.get('title', instance.title)
+#         instance.storyline = validate_data.get('storyline', instance.storyline)
+#         instance.active = validate_data.get('active', instance.active)
+#         instance.created_date = validate_data.get('created_date', instance.created_date)
+#         instance.save()
+#         return instance
     
-    # Field level Validation
-    def validate_title(self, value):
-        if len(value) < 3:
-            raise serializers.ValidationError("Title length is too short.")
-        return value
+#     # Field level Validation
+#     def validate_title(self, value):
+#         if len(value) < 3:
+#             raise serializers.ValidationError("Title length is too short.")
+#         return value
     
     
-    # Validation on multiple fields
-    def validate(self, data):
-        if data['title'].lower() == data['storyline'].lower():
-            raise serializers.ValidationError("Title and its Description should be Different.")
-        return data
+#     # Validation on multiple fields
+#     def validate(self, data):
+#         if data['title'].lower() == data['storyline'].lower():
+#             raise serializers.ValidationError("Title and its Description should be Different.")
+#         return data
